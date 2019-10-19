325ab53cd0c276cd6db706c89bb979288ff0cc25
Anastasia Pankina <siapankina@gmail.com>
2019-10-19 10:02:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,12 @@
 require "yaml"
 
 def load_library(file_path)
- YAML.load_file(file_path)
+ emojis=YAML.load_file(file_path)
  total={
    :get_meaning=>{}
    :get_emoticon=>{}
  }
- emoji.each{|emotion,symbols|
+ emojis.each{|emotion,symbols|
  total[:get_meaning].store(symbols[0],symbols[1])
  total[:get_emoticon].store(symbols[1],symbols[0])
 end
