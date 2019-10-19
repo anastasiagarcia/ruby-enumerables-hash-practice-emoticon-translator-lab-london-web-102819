708396897aa9ae59181fec2caffd210664541fa1
Anastasia Pankina <siapankina@gmail.com>
2019-10-19 10:02:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(file_path)
    :get_emoticon=>{}
  }
  emoji.each{|emotion,symbols|
- total[get_meaning].store(symbols[0],symbols[1])
- total[get_emoticon].store(symbols[1],symbols[0])
+ total[:get_meaning].store(symbols[0],symbols[1])
+ total[:get_emoticon].store(symbols[1],symbols[0])
 end
 
 def get_japanese_emoticon
