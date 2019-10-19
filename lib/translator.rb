de713969a72eb45d45e6fb772d5254b674787b42
Anastasia Pankina <siapankina@gmail.com>
2019-10-19 10:30:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,5 +27,5 @@ def get_english_meaning(file_path, emoticon)
   if translations[:get_meaning].key?(emoticon)
     return translations[:get_meaning][emoticon]
   end
-  return "Sorry, the emoticon was not found"
+  return "Sorry, that emoticon was not found"
 end
