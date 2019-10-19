066c357a21d677b069c657aa28f37830c434a0ce
Anastasia Pankina <siapankina@gmail.com>
2019-10-19 10:04:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(file_path)
    :get_emoticon=>{}
  }
  emojis.each{|emotion,symbols|
-  total[:get_meaning].store(symbols[0],symbols[1])
-  total[:get_emoticon].store(symbols[1],symbols[0])
+  total[:get_meaning].store(symbols[1],symbols[0])
+  total[:get_emoticon].store(symbols[0],symbols[1])
  }
  total
 end
