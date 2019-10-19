6f82774e25330031a2636b8a5bd42feb9562823d
Anastasia Pankina <siapankina@gmail.com>
2019-10-19 10:25:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if translations[:get_emoticon].key?(emoticon)
     return translations[:get_emoticon][emoticon]
   end
+  return "Sorry, that emoticon was not found"
   
 end
 
